6dc65b48ba64bd1499ed80286430ca0ce783bc8f
pbrodriguezm <pb.rodriguez.m@gmail.com>
2015-10-06 13:12:39 -0500

    Create exerc-5.cpp
    5.- Write a C function that includes the following sequence of statements

---
+++ exerc-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>     
+#include <fstream>      
+
+int x= 21;
+int funcion() {
+   int x= 42;
+   return 0;
+}
+
+int main () {
+ funcion();
+  return 0;
+}
+
+/*5.- Write a C function that includes the following sequence of statements:*/

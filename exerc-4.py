f1f68a518ec6eaa164c2b6b5528e1aa74001aeb5
pbrodriguezm <pb.rodriguez.m@gmail.com>
2015-10-06 13:11:10 -0500

    Create exerc-4.py
    4.-en python repetir la pregunta 3

---
+++ exerc-4.py
@@ -0,0 +1,12 @@
+"""
+4.-en python repetir la pregunta 3 en python
+"""
+# bueno aqui al igual que en Js se crea variables globales y varibales locales y es lamda por una subfuncion internet "print"
+x=1
+def add(x, y):
+    x = 42
+    y = 21
+    print(x,y)
+x,y = 3,4
+add(17,4)
+print(x,y)

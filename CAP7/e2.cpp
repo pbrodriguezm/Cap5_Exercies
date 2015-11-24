ac8a13a61da62a5402b29624f219eb2013bcfc4e
pbrodriguezm <pb.rodriguez.m@gmail.com>
2015-11-23 19:21:17 -0500

    Update e2.cpp

--- CAP7/e2.cpp
+++ CAP7/e2.cpp
@@ -1,19 +1,16 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
-
-//c++
-int fun (int *k){
-    *k += 4;
-    return 3 * (*k) - 1;
+int fun(int *k) {
+	*k += 4;
+	return 3 * (*k) - 1;
 }
 
-int main()
-{
-    int i = 10, j = 10, sum1, sum2;
-    sum1= (i / 2) + fun( &i );
-    sum2 = fun ( &j ) + (j / 2);
-    
-    cout << "Hello world!->" << sum1<<endl;
-    return 0;
+int main() {
+	int i = 10, j = 10, sum1, sum2;
+	sum1 = (i / 2) + fun(&i);
+	sum2 = fun(&j) + (j / 2);
+	printf("sum1: %i\n",sum1);
+	printf("sum2: %i\n",sum2);
+	return 0;
 }
+//sum1 46 sum2 48
